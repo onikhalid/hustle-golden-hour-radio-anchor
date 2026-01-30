b369961614481c1f63dd9415261e43a2ddbf856d
Khalid Oni <onikhalidayo@gmail.com>
2026-01-30 13:48:39 +0100

    chore: remove a deleted file.

--- app/(main)/streams/misc/api/index.ts
+++
@@ -1,9 +0,0 @@
-export { useCreateStreamSession } from "./postCreateStreamSession";
-export { useGetStreamSessions } from "./getStreamSessions";
-export { useInitSingleUpload } from "./postInitSingleUpload";
-export { useCompileStreamSession } from "./postCompileStreamSession";
-export { useCompleteUpload } from "./postCompleteUpload";
-export { useStartLiveSession } from "./postStartLiveSession";
-export { usePushStreamQuestion } from "./postPushStreamQuestion";
-export { usePushQuestionsTime } from "./postPushQuestionsTime";
-export { usePushQuestionEndTimer } from "./postPushQuestionEndTimer";
