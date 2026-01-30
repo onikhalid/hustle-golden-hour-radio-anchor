9fa78c73c94c4645d507e89d0e7f7c4155574290
Khalid Oni <onikhalidayo@gmail.com>
2026-01-30 13:58:02 +0100

    feat: implement a new login page with credentials authentication and UI.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
   const [error, setError] = useState<string | null>(null);
   const credentialsLoginMutation = useCredentialsLogin();
   const { authDispatch } = useAuth();
-  const router = useRouter()
+  const router = useRouter();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
@@ -31,13 +31,14 @@ export default function LoginPage() {
         setAxiosDefaultToken(data.token, gameAxios);
         router.push("/");
       },
-      onError: (err) => { 
+      onError: (err) => {
         setError(formatAxiosErrorMessage(err as AxiosError) ?? "");
       },
     });
   };
 
-  const modalCardClasses = "rounded-3xl border border-white/10 bg-white/[0.07] p-8 shadow-[0_40px_120px_-60px_rgba(5,0,20,0.9)] backdrop-blur-xl";
+  const modalCardClasses =
+    "rounded-3xl border border-white/10 bg-white/[0.07] p-8 shadow-[0_40px_120px_-60px_rgba(5,0,20,0.9)] backdrop-blur-xl";
 
   return (
     <div className="relative min-h-screen overflow-hidden bg-[url('/images/bg/hustle-bg.png')] bg-cover bg-center">
@@ -46,16 +47,24 @@ export default function LoginPage() {
       <div className="relative z-10 flex min-h-screen items-center justify-center px-4 py-16">
         <div className={`${modalCardClasses} w-full max-w-lg text-white/90`}>
           <header className="flex flex-col gap-2 pr-16">
-            <span className="text-xs uppercase tracking-[0.3em] text-white/50">Welcome back</span>
-            <h1 className="text-2xl font-semibold text-white">Admin control access</h1>
+            <span className="text-xs uppercase tracking-[0.3em] text-white/50">
+              Welcome back
+            </span>
+            <h1 className="text-2xl font-semibold text-white">
+              Admin control access
+            </h1>
             <p className="text-sm text-white/60">
-              Sign in to create sream sessions compile video recordings, spin up live quiz sessions, monitor contestants, and crown your winners in real time.
+              Sign in to create and manage your station's questions and question
+              categories, spin up live quiz sessions, monitor contestants, and
+              crown your winners in real time.
             </p>
           </header>
 
           <form className="mt-8 space-y-6" onSubmit={handleLogin}>
             <div className="space-y-2">
-              <label className="text-xs uppercase tracking-[0.2em] text-white/50">Phone or email</label>
+              <label className="text-xs uppercase tracking-[0.2em] text-white/50">
+                Phone or email
+              </label>
               <input
                 value={phoneOrEmail}
                 onChange={(e) => setPhoneOrEmail(e.target.value)}
@@ -65,7 +74,9 @@ export default function LoginPage() {
             </div>
 
             <div className="space-y-2">
-              <label className="text-xs uppercase tracking-[0.2em] text-white/50">Password</label>
+              <label className="text-xs uppercase tracking-[0.2em] text-white/50">
+                Password
+              </label>
               <input
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -85,9 +96,10 @@ export default function LoginPage() {
               type="submit"
               disabled={credentialsLoginMutation.isPending}
               className="cursor-pointer rounded-full w-full border border-white/20 bg-[#22C55E]/80 px-4 py-3 text-xs uppercase tracking-[0.2em] text-[#FFFFFF] transition hover:bg-white/20 mt-8 disabled:opacity-45"
-              
             >
-              {credentialsLoginMutation.isPending ? "Logging in..." : "Enter dashboard"}
+              {credentialsLoginMutation.isPending
+                ? "Logging in..."
+                : "Enter dashboard"}
             </button>
           </form>
 
